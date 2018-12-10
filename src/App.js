f8751d84d82910b864528269b5cf5b02dca9b158
Gagan Sikri <sikrigagan@sikrigagan-MacBook-Pro.local>
2018-12-11 01:50:42 +0530

    Google Sheets API Implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import config from './config';
 import './App.css';
 
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${ config.spreadsheetId }/values:batchGet?ranges=Sheet1&majorDimension=${ config.dimension }&key=${ config.apiKey }`;
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+     items:[]
+    };
+  }
+  
+  componentDidMount() {
+    fetch(url).then(response => response.json()).then(data => {
+      let batchRowValues = data.valueRanges[0].values;
+ 
+      const rows = [];
+      for (let i = 1; i < batchRowValues.length; i++) {
+        let rowObject = {};
+        for (let j = 0; j < batchRowValues[i].length; j++) {
+          rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
+        }
+        rows.push(rowObject);
+      }
+ 
+      this.setState({ items: rows });
+    });
+  }
+  
   render() {
+    const listItems = this.state.items.map((item) =>
+      <li>Net revenue for {item.month} was {item.net_revenue} with gross revenue and coupon spends of {item.gross_revenue} and {item.coupon_spend}({item.coupon_share}) respectively. <br/>Churn rate was {item.churn}; with {item.renewed_users} renewals and {item.new_users} new users. While, ARPU was {item.arpu}; CAC was {item.cac} and LTV was {item.ltv}</li>
+    ); 
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+         <ul>{listItems}</ul>
       </div>
     );
   }
