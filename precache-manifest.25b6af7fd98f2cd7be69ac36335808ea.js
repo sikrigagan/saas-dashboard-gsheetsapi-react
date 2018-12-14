8ac93a7569eb53f2f652dd2d84ef396381f7e6ca
Gagan Sikri <sikrigagan@sikrigagan-MacBook-Pro.local>
2018-12-14 20:31:54 +0530

    Created Github Page

---
+++ precache-manifest.25b6af7fd98f2cd7be69ac36335808ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e6175a29922df9ac5ba1",
+    "url": "/static/js/main.e6175a29.chunk.js"
+  },
+  {
+    "revision": "611a442bd5f50bc719d7",
+    "url": "/static/js/1.611a442b.chunk.js"
+  },
+  {
+    "revision": "e6175a29922df9ac5ba1",
+    "url": "/static/css/main.22eb436e.chunk.css"
+  },
+  {
+    "revision": "611a442bd5f50bc719d7",
+    "url": "/static/css/1.84b57e09.chunk.css"
+  },
+  {
+    "revision": "220c3ef5a7d90020f52399b3b8de24a4",
+    "url": "/index.html"
+  }
+];
--- src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    apiKey: 'AIzaSyDB5BYikj0A6-zvEdDFWFZ87BYzC1wOK80',
-    spreadsheetId: '1kVwf5oIZMPqY4Y2WRj9NzeDgy3zBBj-px-Hl41sFlT8'
-}
